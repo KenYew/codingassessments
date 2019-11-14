6b4cbe93b2525b161bcbaf1112098a5a555b632e
Your Name <you@example.com>
2019-11-14 21:06:44 +0000

    amazon

---
+++ Amazon/Amazon.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class Amazon {
+    // practice Q1:
+    // There is a colony of 8 cells arranged in a straight line where each day every cell competes with its adjacent cells (neighbours).
+    // Each day, for each cell, if its neighbours are both active or both inactive, the cell becomes inactive the next day;
+    // otherwise it becomes active the next day
+
+    public static int cellComplete(int[] cells, int days) {
+        return 0;
+    }
+
+    // practice Q2:
+    // The Greatest Common Divisor (also called Highest Common Factor) of n numbers is the largets positive integer
+    // that divides all numbers without a remainder.
+    // Write a method generalisedGCD of class GCD to find the GCD of n positive integers.
+
+    // input:
+    // - array arr of positive integers
+    // output:
+    // - GCD of the array of integers
+
+    public static int generalisedGCD(int arr[]) {
+        return 0;
+    }
+
+    // Q1:
+    // Amazon prides itself on protecting customers and sellers from fraud. Amazon assigns fraud ratings to actions taken on the website.
+    // We scan these rating values to try and determine when to accept a given request. Fraud ratings are assigned in a complementary manner.
+    // When two ratings add up to a certain value, it increases the risk that the request may be fraudulent.
+    // Write an algorithm to find the number of distinct pair of ratings from an array of all fraud ratings for the request that sum to a target value
+
+    // Q2:
+    // Amazon wants to pilet hardware replacements in Data Centre AMZN525, to increase the reliability of their network.
+    // AMZN525 has a "connected" network of data servers, i.e. every server can communicate with the rest of the network.
+    // The data servers are connected by point-to-point cables to establish "critical" or "non-critical" connections.
+    // A connection is considered "critical" if its removal results in a disconnected network of servers.
+    // Amazon wants to significantly increase the reliability of its Data Centers by replacing the critical connections with far more reliable cables.
+    // Write a method that returns the critical connections in AMZN525
+
+    // input:
+    // 1) - numOfServers
+    // 2) - numOfConnections
+    // 3) - connections
+
+    // output:
+    // - Return a list of integer pairs representing the critical connections. Output an empty list 
+
+    public static void main(String[] args) {
+        System.out.println("boo");
+    }
+}
